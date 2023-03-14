927ed52b569bde4bf604f9a84a036fb8056ba530
DilaraKaraca51 <dilarakaraca2004@gmail.com>
2023-03-14 15:07:54 +0300

    AsalSayılarÖrnek
    Project has been created

---
+++ AsalSayilar/AsalSayilar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.Write("Sayı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                    sayac++;
+            }
+            if (sayac != 0)
+            { Console.WriteLine("Girilen sayı asal değil"); }
+            else
+            { Console.WriteLine("Girilen sayı asaldır"); }
+        
+            
+            Console.ReadKey();
+        }
+    }
+}
